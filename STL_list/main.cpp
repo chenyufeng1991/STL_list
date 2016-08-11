5c5d44be0a8bd7f1a3b7a77424e85cfbb8dec8b0
陈宇峰 <yufengcode@gmail.com>
2016-08-11 19:54:51 +0800

    list构造函数。

--- STL_list/main.cpp
+++ STL_list/main.cpp
@@ -7,9 +7,25 @@
 //
 
 #include <iostream>
+#include <list>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+/**
+ *  list是一个双向链表，不能进行随机访问
+ *
+ */
+int main(int argc, const char * argv[])
+{
+    list<int> list1; // 创建一个空的list
+    list<int> list2(list1); // 复制另一个同类型元素的list
+    list<int> list3(10); // 创建n个元素的list，每个元素值由默认构造函数确定
+    list<int> list4(10,0); // 创建n个元素的list，每个元素的值为elem
+
+
+
+
     return 0;
 }
+
+
